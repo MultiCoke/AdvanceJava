ba06198b97525b01c44634243eed322a868e68b3
xiaojianhua <xiaojh_learn@163.com>
2021-03-12 22:49:36 +0800

    Ready work

---
+++ jdk/src/com/upmerge/test/HashMapTest.java
@@ -0,0 +1,14 @@
+package com.upmerge.test;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String, Double> hashMap = new HashMap<>();
+
+        hashMap.put("k1", 0.01);
+        hashMap.put("k2", 0.02);
+        hashMap.put("k3", 0.03);
+        hashMap.put("k4", 0.04);
+    }
+}
